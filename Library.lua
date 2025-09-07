ca2abe72b0b28383a2572ffd86e720f80c7bd2cc
Agnes Tachyon <89581874+0xCiel@users.noreply.github.com>
2025-09-08 06:37:37 +0900

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -49,7 +49,7 @@ local Library = {
 
     Notifications = {},
 
-    ToggleKeybind = Enum.KeyCode.F1,
+    ToggleKeybind = Enum.KeyCode.RightControl,
     TweenInfo = TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
     NotifyTweenInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
 
